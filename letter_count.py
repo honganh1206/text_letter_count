73768c897b87b18d44cb79c6016a6d67b82cf0be
Hugh Pham <79431746+honganh1206@users.noreply.github.com>
2021-06-16 12:52:20 +0700

    Update letter_count.py

--- letter_count.py
+++ letter_count.py
@@ -10,26 +10,27 @@ def main():
 
 def read_data():
     word_dict = {}
-    file = Path('automateboringstuff\The Picture of Dorian Gray.txt').read_text(encoding="utf8")
-    for char in file:                                   # counting each letter in the text
+    file = Path('insert Path to file here.txt').read_text(encoding="utf8")
+    for char in file:                                  
         word_dict.setdefault(char, 0) 
-        word_dict[char] += 1                               # word_dict become a DICT here
+        word_dict[char] += 1                               
     return word_dict
 
-def create_bar_plot(count_num):                             # param is a dict
+def create_bar_plot(count_num):                             
 
     alphabet_list = list(string.ascii_lowercase)            # _letters for both uppercase and lowercase
     filtered_dict = {}
 
-    for key, value in sorted(count_num.items()):            # need .items() for 2 VARS (keys:values) + keyword SORTED
+    for key, value in sorted(count_num.items()):            
         if key in alphabet_list:
             filtered_dict[key] = value
-
-    counts = []                             # take the numbers of times each letter appears and add them to a list
+            
+    # count letters
+    counts = []                           
     for letter in filtered_dict:
         counts.append(filtered_dict[letter])
-    
-
+        
+    # visualize data
     data = {
         'x':list(filtered_dict.keys()),
         'y':counts
